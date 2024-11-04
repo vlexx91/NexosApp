2bdf8c72cc0a50f014221a5b59b424f5a6865141
Alex <amartinenghivelazquez@safareyes.es>
2024-11-04 20:13:09 +0100

    Initial commit

---
+++ src/main/java/com/example/nexosapp/NexosAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.nexosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexosAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexosAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nexosapp/NexosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nexosapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexosAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
