102012c68b27dad4e21d8c83c1cabfc212ddd26c
Alex <amartinenghivelazquez@safareyes.es>
2024-11-06 18:39:22 +0100

    crear carpeta

---
+++ src/main/java/com/example/nexosapp/controladores/LugarControlador.java
@@ -0,0 +1,10 @@
+package com.example.nexosapp.controladores;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/lugar")
+public class LugarControlador {
+
+}
