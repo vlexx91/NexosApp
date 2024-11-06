d05f66d272739abd072d55861c86f1e4ad4d3af3
Alex <amartinenghivelazquez@safareyes.es>
2024-11-06 19:56:41 +0100

    LugarControlador, LugarRepositorio creado, LugarServicio con metodos basicos

--- src/main/java/com/example/nexosapp/controladores/LugarControlador.java
+++ src/main/java/com/example/nexosapp/controladores/LugarControlador.java
@@ -1,10 +1,61 @@
 package com.example.nexosapp.controladores;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.nexosapp.modelos.Lugar;
+import com.example.nexosapp.servicios.LugarServicio;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/lugar")
+@AllArgsConstructor
 public class LugarControlador {
 
+    private LugarServicio lugarServicio;
+
+    /**
+     * obtiene todos los lugares
+     * @return
+     */
+
+    @GetMapping("/lista")
+    public List<Lugar> getAllLugares(){
+        return lugarServicio.getLugares();
+    }
+
+    /**
+     * obtiene lugar por id
+     * @param id
+     * @return
+     */
+
+    @GetMapping("/obtener/{id}")
+    public Lugar getLugarId(@PathVariable Integer id){
+        Lugar lugar = lugarServicio.getLugarId(id);
+        return lugar;
+    }
+
+    /**
+     * guarda o edita un lugar
+     * @param lugar
+     * @return
+     */
+
+    @PostMapping("/guardar")
+    public Lugar guardar(@RequestBody Lugar lugar){
+        lugar = lugarServicio.guardar(lugar);
+        return lugar;
+    }
+
+
+    @DeleteMapping("/eliminar")
+    public String eliminar(@RequestParam("id") Integer id){
+        try {
+            lugarServicio.eliminaLugarId(id);
+            return "lugar eliminado";
+        } catch (Exception e){
+            return "imposible eliminar lugar";
+        }
+    }
 }
