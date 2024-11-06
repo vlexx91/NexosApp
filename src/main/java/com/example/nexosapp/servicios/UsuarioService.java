f676012e46d3d7c46fd69f9dd97ce614973ed496
daniel <dnunezalvarez@safareyes.es>
2024-11-06 19:55:54 +0100

    services y controller de usuario

--- src/main/java/com/example/nexosapp/servicios/UsuarioService.java
+++ src/main/java/com/example/nexosapp/servicios/UsuarioService.java
@@ -1,12 +1,73 @@
 package com.example.nexosapp.servicios;
 
+import com.example.nexosapp.modelos.Usuario;
 import com.example.nexosapp.repositorios.UsuarioRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UsuarioService {
 
    @Autowired
     private UsuarioRepositorio usuarioRepositorio;
+
+    /**
+     * Obtener todos los Usuarios
+     * @return
+     */
+
+    public List<Usuario>getUsuarios(){
+        return usuarioRepositorio.findAll();
+    }
+
+    /**
+     * Obtener usuario por id
+     * @return
+     */
+
+    public Usuario getUsuarioId(Integer id){
+        return usuarioRepositorio.findById(id).orElse(null);
+    }
+
+
+    /**
+     * Crea o edita un  usuario
+     * @return
+     */
+
+    public Usuario guardar(Usuario usuario){
+        return usuarioRepositorio.save(usuario);
+    }
+
+    /**
+     *Elimina un usuario por id
+     * @return
+     */
+
+    public String eliminaUsuarioId(Integer id) {
+        String mensaje;
+        Usuario usuario = usuarioRepositorio.findById(id).orElse(null);
+
+        if (usuario == null) {
+            mensaje = "Ese usuario no existe";
+            return mensaje;
+        }
+
+        try {
+            usuarioRepositorio.deleteById(id);
+            Usuario usuariotry = usuarioRepositorio.findById(id).orElse(null);
+
+            if (usuariotry != null) {
+                mensaje = "No se ha podido eliminar el usuario";
+            } else {
+                mensaje = "Usuario eliminado";
+            }
+        } catch (Exception e) {
+            mensaje = "No se ha podido eliminar el usuario";
+        }
+
+        return mensaje;
+    }
 }
