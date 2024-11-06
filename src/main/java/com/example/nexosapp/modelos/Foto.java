be191d31e5263372a6d1f3401fe6ad28af354201
rafdiecas <rdiezcastro@safareyes.es>
2024-11-06 18:46:56 +0100

    add- modelo Foto.java

---
+++ src/main/java/com/example/nexosapp/modelos/Foto.java
@@ -0,0 +1,23 @@
+package com.example.nexosapp.modelos;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="foto", schema="nexo_app",catalog = "postgres")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Foto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "url",nullable = false)
+    private String url;
+    @Column(name = "es_cara",nullable = false)
+    private Boolean esCara;
+}
