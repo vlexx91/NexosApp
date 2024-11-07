78634d10742bc487156925649d86d3276b475dfe
daw <lbenturayuca@safareyes.es>
2024-11-07 16:38:41 +0100

    DTO Aviso

--- src/main/java/com/example/nexosapp/DTO/AvisoDTO.java
+++ src/main/java/com/example/nexosapp/DTO/AvisoDTO.java
@@ -1,7 +1,18 @@
 package com.example.nexosapp.DTO;
 
-import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AvisoDTO {
     private LocalDate fecha;
+    private String texto;
+    private Integer usuarioId;
+    private Set<FotoUrlDTO> fotos;
 }
