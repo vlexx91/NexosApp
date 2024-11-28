0acc594f110e552feff841c333289f1fd9435b37
Alex <amartinenghivelazquez@safareyes.es>
2024-11-28 19:30:19 +0100

    listaAdminDTO importada

--- src/main/java/com/example/nexosapp/servicios/UsuarioService.java
+++ src/main/java/com/example/nexosapp/servicios/UsuarioService.java
@@ -1,6 +1,7 @@
 package com.example.nexosapp.servicios;
 
 import com.example.nexosapp.DTO.DesaparicionPrincipalDTO;
+import com.example.nexosapp.DTO.UsuarioAdminListaDTO;
 import com.example.nexosapp.modelos.Civil;
 import com.example.nexosapp.modelos.Desaparicion;
 import com.example.nexosapp.modelos.Usuario;
